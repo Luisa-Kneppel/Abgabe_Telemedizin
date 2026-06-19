0fb8094398eb697e702fa34aab41e0621d3c5d80
Luisa-Kneppel <kl9704@mci4me.at>
2026-06-19 10:44:50 +0200

    arzt test

---
+++ arzt.py
@@ -0,0 +1,16 @@
+from read_data import load_person_data, get_person_list
+from patienten import get_person_object_by_full_name
+
+patienten_data = load_person_data()
+person_names = get_person_list(patienten_data)
+
+selected_person = st.selectbox("Patient:in auswählen", person_names)
+
+patient = get_person_object_by_full_name(selected_person)
+
+st.write("Name: " + patient.get_full_name())
+st.write("Alter: " + str(patient.calc_age()))
+st.write("Telefon: " + patient.telefon)
+st.write("Adresse: " + patient.get_adresse_as_string())
+st.write("Diagnosen: " + patient.get_diagnosen_as_string())
+st.write("Medikamente: " + patient.get_medikamente_as_string())
--- main.py
+++ main.py
@@ -54,19 +54,3 @@ if col1.button("Arzt", use_container_width=True):
     st.session_state["rolle"] = "arzt"
     st.switch_page("pages/arzt.py")
 
-from read_data import load_person_data, get_person_list
-from patienten import get_person_object_by_full_name
-
-patienten_data = load_person_data()
-person_names = get_person_list(patienten_data)
-
-selected_person = st.selectbox("Patient:in auswählen", person_names)
-
-patient = get_person_object_by_full_name(selected_person)
-
-st.write("Name: " + patient.get_full_name())
-st.write("Alter: " + str(patient.calc_age()))
-st.write("Telefon: " + patient.telefon)
-st.write("Adresse: " + patient.get_adresse_as_string())
-st.write("Diagnosen: " + patient.get_diagnosen_as_string())
-st.write("Medikamente: " + patient.get_medikamente_as_string())
